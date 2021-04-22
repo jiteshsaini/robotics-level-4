51231aeee46ed1f8f4756b9f551f226289024e5c
Jitesh <46092054+jiteshsaini@users.noreply.github.com>
2021-04-22 22:40:06 +0530

    Update util.py

--- earthrover/util.py
+++ earthrover/util.py
@@ -61,10 +61,10 @@ def forward():
 	GPIO.output(m2_2, True)
 	
 def stop():
-	GPIO.output(m1_1, True)
-	GPIO.output(m1_2, True)
-	GPIO.output(m2_1, True)
-	GPIO.output(m2_2, True)
+	GPIO.output(m1_1, False)
+	GPIO.output(m1_2, False)
+	GPIO.output(m2_1, False)
+	GPIO.output(m2_2, False)
 
 def speak_tts(text,gender):
 	cmd="python /var/www/html/earthrover/speaker/speaker_tts.py '" + text + "' " + gender + " &"
