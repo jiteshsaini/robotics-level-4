469cc666446ef7984ee66508183e63f3eeb3f48e
Jitesh <46092054+jiteshsaini@users.noreply.github.com>
2021-05-08 17:59:50 +0530

    ..

--- earthrover/control_panel/misc/hw.py
+++ earthrover/control_panel/misc/hw.py
@@ -1,11 +1,11 @@
 from urllib import urlopen
-import sys
+import sys, commands
 
 entry_by = sys.argv[1]
-url_local = sys.argv[2]
+web_page = sys.argv[2]
 
-url_remote="https://helloworld.co.in/earthrover/run.php?entry_by=" + entry_by + "&url=" + url_local
+ip_local=commands.getoutput('hostname -I')
+
+url_remote="https://helloworld.co.in/earthrover/run.php?entry_by=" + entry_by + "&url=" + web_page + "&ip_local=" + ip_local
 
 urlopen(url_remote)
-
-print (">>>>")
