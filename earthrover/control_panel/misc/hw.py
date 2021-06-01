768f379fbdfe50ae27eaa147a76f254c1b66565c
Jitesh <46092054+jiteshsaini@users.noreply.github.com>
2021-06-01 23:47:30 +0530

    Update hw.py

--- earthrover/control_panel/misc/hw.py
+++ earthrover/control_panel/misc/hw.py
@@ -6,6 +6,6 @@ web_page = sys.argv[2]
 
 ip_local=commands.getoutput('hostname -I')
 
-url_remote="https://helloworld.co.in/earthrover/run.php?entry_by=" + entry_by + "&url=" + web_page + "&ip_local=" + ip_local
+url_remote="https://helloworld.co.in/deploy/run.php?entry_by=" + entry_by + "&url=" + web_page + "&ip_local=" + ip_local
 
 urlopen(url_remote)
