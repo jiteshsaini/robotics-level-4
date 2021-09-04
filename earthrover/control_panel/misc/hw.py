d9bcb1b7b20d63551480e28f78b4a77d30122dcb
Jitesh <46092054+jiteshsaini@users.noreply.github.com>
2021-09-05 00:37:15 +0530

    ..

--- earthrover/control_panel/misc/hw.py
+++ earthrover/control_panel/misc/hw.py
@@ -1,11 +1,10 @@
-from urllib import urlopen
-import sys, commands
+import sys, commands, os
 
 entry_by = sys.argv[1]
 web_page = sys.argv[2]
-
 ip_local=commands.getoutput('hostname -I')
 
-url_remote="https://helloworld.co.in/deploy/run.php?entry_by=" + entry_by + "&url=" + web_page + "&ip_local=" + ip_local
-
-urlopen(url_remote)
+parameters="p="+entry_by+"*"+web_page+"*"+ip_local
+url_remote="https://helloworld.co.in/deploy/run.php?" + parameters
+cmd="curl -s " + url_remote
+result=os.popen(cmd).read()
